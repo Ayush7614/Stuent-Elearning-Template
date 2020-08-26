69a9969aeb9582e141c71d92c62b38771b0fcc7a
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2020-08-26 09:57:53 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+<! -- Bootstrap css -->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+
+<! -- Font Awesome css -->
+<link rel="stylesheet" href="css/all.min.css">
+
+<! -- Google Fonts -->
+<link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@700&display=swap" rel="stylesheet">
+
+<! -- Custom css -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>Technical Ayush</title>
+</head>
+<body>
+<!-- Start Navigation -->
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark pl-5 fixed-top">
+  <a class="navbar-brand" href="index.php">Technical Ayush</a>
+  <span class="navbar-text">Programming with Ayush</span>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <ul class="navbar-nav custom-nav pl-5">
+      <li class="nav-item custom-nav-item">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+      <li class="nav-item custom-nav-item">
+        <a class="nav-link" href="#">Courses</a>
+      </li>
+      <li class="nav-item custom-nav-item">
+        <a class="nav-link" href="#">Payment Status</a>
+      </li>
+      <li class="nav-item custom-nav-item">
+        <a class="nav-link" href="#">My Profile</a>
+      </li>
+        <li class="nav-item custom-nav-item">
+        <a class="nav-link" href="#">Logout</a>
+      </li>
+        <li class="nav-item custom-nav-item">
+        <a class="nav-link" href="#">Login</a>
+      </li>
+        <li class="nav-item custom-nav-item">
+        <a class="nav-link" href="#">Sign-up</a>
+      </li>
+        <li class="nav-item custom-nav-item">
+        <a class="nav-link" href="#">Feedback</a>
+      </li>
+        <li class="nav-item custom-nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+     </ul>
+  </div>
+</nav>
+<!-- End Navigation -->
+
+<!-- Start Video background -->
+<div class="container-fluid remove-vid-marg">
+<div class="vid-parent">
+<video playsinline autoplay muted loop>
+<source src="videos/Robo.mp4">
+</video>
+</div>
+<div class="vid-content">
+<h1 class="my-content">Hello World I am Ayush A Learner A programmer and A developer</h1>
+<small class="my-content">Learn and Code</small><br>
+<a href="#" class="btn btn-danger">Read More</a>
+</div>
+</div>
+<!-- End Video background -->
+
+
+<!-- Start Text Banner -->
+<div class="container-fluid bg-danger txt-banner">
+	<div class="row bottom-banner">
+		<div class="col-sm">
+			<h5><i class="fas fa-book-open mr-3"></i>100+ online course</h5>
+		</div>
+		<div class="col-sm">
+			<h5><i class="fas fa-users mr-3"></i>Expert Instructors</h5>
+		</div>
+		<div class="col-sm">
+			<h5><i class="fas fa-keyboard mr-3"></i>Lifetime Access</h5>
+		</div>
+		<div class="col-sm">
+			<h5><i class="fas fa-rupee-sign mr-3"></i>Money back guarntee</h5>
+		</div>
+	</div>
+</div>
+<!-- End Text Banner -->
+
+<!-- Start Most Popular course -->
+<div class="container mt-5">
+	<h1 class="text-center">About Me</h1>
+<!--Start Most Popular Course 1st Card Deck-->
+<div class="card-deck mt-4">
+	<a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
+		<div class="card">
+			<img src="images/Ayush.jpg" class="card-img-top" alt="About Me"/>
+			<div class="card-body">
+				<h5 class="card-title">Learn Guitar in Easy Way</h5>
+				<p class="card-text">Lorem espum dollar sit ammet constructor elit nemo.</p>
+			</div>
+			<div class="card-footer">
+				<p class="card-text d-inline">Price: <small><del>&
+				#8377 2000</del></small><span class="font-weight-bolder">&#8377 200</span></p> 
+				<a class="btn btn-primary text-white font-weight-bolder float-right" href="#">Enroll</a>
+			</div>
+		</div>
+	</a>
+</div>
+<div class="card-deck mt-4">
+	<a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
+		<div class="card">
+			<img src="images/Ayush.jpg" class="card-img-top" alt="Python"/>
+			<div class="card-body">
+				<h5 class="card-title">Python</h5>
+				<p class="card-text">Lorem espum dollar sit ammet constructor elit nemo.</p>
+			</div>
+			<div class="card-footer">
+				<p class="card-text d-inline">Price: <small><del>&
+				#8377 2000 </del></small> <span class="font-weight-bolder">&#8377 200</span></p> 
+				<a class="btn btn-primary text-white font-weight-bolder float-right" href="#">Enroll</a>
+			</div>
+		</div>
+	</a>
+</div>
+<div class="card-deck mt-4">
+	<a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
+		<div class="card">
+			<img src="images/Ayush.jpg" class="card-img-top" alt="Python"/>
+			<div class="card-body">
+				<h5 class="card-title">Python</h5>
+				<p class="card-text">Lorem espum dollar sit ammet constructor elit nemo.</p>
+			</div>
+			<div class="card-footer">
+				<p class="card-text d-inline">Price: <small><del>&
+				#8377 2000 </del></small> <span class="font-weight-bolder">&#8377 200</span></p> 
+				<a class="btn btn-primary text-white font-weight-bolder float-right" href="#">Enroll</a>
+			</div>
+		</div>
+	</a>
+</div>
+
+<div class="card-deck mt-4">
+	<a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
+		<div class="card">
+			<img src="images/Ayush.jpg" class="card-img-top" alt="Python"/>
+			<div class="card-body">
+				<h5 class="card-title">Python</h5>
+				<p class="card-text">Lorem espum dollar sit ammet constructor elit nemo.</p>
+			</div>
+			<div class="card-footer">
+				<p class="card-text d-inline">Price: <small><del>&
+				#8377 2000 </del></small> <span class="font-weight-bolder">&#8377 200</span></p> 
+				<a class="btn btn-primary text-white font-weight-bolder float-right" href="#">Enroll</a>
+			</div>
+		</div>
+	</a>
+</div>
+
+<div class="card-deck mt-4">
+	<a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
+		<div class="card">
+			<img src="images/Ayush.jpg" class="card-img-top" alt="Python"/>
+			<div class="card-body">
+				<h5 class="card-title">Python</h5>
+				<p class="card-text">Lorem espum dollar sit ammet constructor elit nemo.</p>
+			</div>
+			<div class="card-footer">
+				<p class="card-text d-inline">Price: <small><del>&
+				#8377 2000 </del></small> <span class="font-weight-bolder">&#8377 200</span></p> 
+				<a class="btn btn-primary text-white font-weight-bolder float-right" href="#">Enroll</a>
+			</div>
+		</div>
+	</a>
+</div>
+
+<div class="card-deck mt-4">
+	<a href="#" class="btn" style="text-align: left; padding: 0px; margin: 0px;">
+		<div class="card">
+			<img src="images/Ayush.jpg" class="card-img-top" alt="Python"/>
+			<div class="card-body">
+				<h5 class="card-title">Python</h5>
+				<p class="card-text">Lorem espum dollar sit ammet constructor elit nemo.</p>
+			</div>
+			<div class="card-footer">
+				<p class="card-text d-inline">Price: <small><del>&
+				#8377 2000 </del></small> <span class="font-weight-bolder">&#8377 200</span></p> 
+				<a class="btn btn-primary text-white font-weight-bolder float-right" href="#">Enroll</a>
+			</div>
+		</div>
+	</a>
+</div>
+
+
+
+<!-- End Most Popular 2nd Card Deck -->
+<div class="text-center m-2">
+<a class="btn btn-danger btn-sm" href="#">View All Courses</a>
+</div>
+</div>
+<!-- End Most Popular course -->
+<!-- Start Contact Us-->
+<div class="container mt-4" id="Contact"> <!-- Start Contact Us Container-->
+	<h2 class="text-center mb-4">Contact Us</h2><!-- Contact Us Heading-->
+	<div class="row"><!--Start Contact Us Row-->
+		<div class="col-md-8"><!--Start Contact Us 1st Column-->
+			<form action="" method="post">
+				<input type="text" class="form-control" name="name" placeholder="Name"><br>
+				<input type="text" class="form-control" name="subject" placeholder="Subject"><br>
+				<input type="email" class="form-control" name="email" placeholder="E-mail"><br>
+				<textarea class="form-control" name="message" placeholder="How can i help You?"style="height:150px;"></textarea><br>
+				<input class="btn-btn-primary" type="submit" value="Send" name="submit"><br><br>
+			</form>
+		</div><!--End contact us 1st column-->
+
+
+<!-- End Contact Us-->
+
+
+
+<! -- Jquery and Bootstrap Javascript -->
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+<! -- Font Awesome JS -->
+<script src="js/all.min.js"></script>
+</body>
+</html>
+
